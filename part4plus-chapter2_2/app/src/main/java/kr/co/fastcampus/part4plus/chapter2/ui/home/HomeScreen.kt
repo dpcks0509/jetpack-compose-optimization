4cda00d914e57b7aef31e1bd001a96cd8cc94314
dpcks0509 <dpcks0509@gmail.com>
2024-12-29 15:56:38 +0900

    feat: Compose 성능 최적화 실습 (2)

--- part4plus-chapter2_2/app/src/main/java/kr/co/fastcampus/part4plus/chapter2/ui/home/HomeScreen.kt
+++ part4plus-chapter2_2/app/src/main/java/kr/co/fastcampus/part4plus/chapter2/ui/home/HomeScreen.kt
@@ -1,17 +1,32 @@
 package kr.co.fastcampus.part4plus.chapter2.ui.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.part4plus.chapter2.model.Memo
 import kr.co.fastcampus.part4plus.chapter2.model.memos
 import kr.co.fastcampus.part4plus.chapter2.ui.theme.MemoAppTheme
@@ -80,7 +95,8 @@ fun ColumnScope.MemoList(onClickAction: (Int) -> Unit, memoList: SnapshotStateLi
             .weight(1f)
     ) {
         items(
-            items = memoList
+            items = memoList,
+            key = { it.id }
         ) { memo ->
             Card(
                 modifier = Modifier
